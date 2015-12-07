065f13209ec0617e6dcb7191a1fc8aafa76540a8
Chloe Horgan <super.chloe@gmail.com>
2015-12-06 21:46:18 -0500

    Finished Algorythm app

---
+++ Algorhythm/PlaylistDetailViewController.h
@@ -0,0 +1,25 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorhythm
+//
+//  Created by Chloe on 2015-10-01.
+//  Copyright © 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Playlist;
+
+@interface PlaylistDetailViewController : UIViewController
+
+@property (strong, nonatomic) Playlist *playlist;
+@property (weak, nonatomic) IBOutlet UIImageView *playlistCoverImage;
+@property (weak, nonatomic) IBOutlet UILabel *playlistTitle;
+@property (weak, nonatomic) IBOutlet UILabel *playlistDescription;
+
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist0;
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist1;
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist2;
+
+
+@end
